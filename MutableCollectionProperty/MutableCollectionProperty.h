e3557f30b0ff9a55cf512a6fcc40c8e44cbc088d
Pedro Pi√±era <pepibumur@gmail.com>
2015-10-14 11:50:43 +0200

    Initial Commit

---
+++ MutableCollectionProperty/MutableCollectionProperty.h
@@ -0,0 +1,19 @@
+//
+//  MutableCollectionProperty.h
+//  MutableCollectionProperty
+//
+//  Created by Pedro Pinera Buendia on 14/10/15.
+//  Copyright © 2015 com.gitdo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MutableCollectionProperty.
+FOUNDATION_EXPORT double MutableCollectionPropertyVersionNumber;
+
+//! Project version string for MutableCollectionProperty.
+FOUNDATION_EXPORT const unsigned char MutableCollectionPropertyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MutableCollectionProperty/PublicHeader.h>
+
+
