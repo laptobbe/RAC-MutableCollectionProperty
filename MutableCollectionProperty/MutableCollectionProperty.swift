01c585c2584005c88f9fbe3ea5e4512238227f5a
Pedro Piñera <pepibumur@gmail.com>
2015-10-15 10:17:02 +0200

    Added deletion and replacement method

--- MutableCollectionProperty/MutableCollectionProperty.swift
+++ MutableCollectionProperty/MutableCollectionProperty.swift
@@ -9,33 +9,91 @@
 import Foundation
 import ReactiveCocoa
 
-enum CollectionChange<T> {
+public enum CollectionChange<T> {
+    case StartChange
+    case EndChange
     case Insertion
-    case Deletion
     case Update
     case Addition
+    case Deletion(Int,T)
+    case Replacement([T])
 }
 
-//public final class MutableCollectionProperty<T>: PropertyType {
-//    
-//    public typealias Value = T
-//    
-//    private let _valueObserver: Signal<[Value], NoError>.Observer
-//    private let _changesObserver: Signal<CollectionChange<Value>, NoError>.Observer
-//    private var _value: [Value]
-//
-//    var producer: SignalProducer<[Value], NoError>
-//    var changes: SignalProducer<CollectionChange<Value>, NoError>
-//    
-//    
-//    public init(_ initialValue: [Value]) {
-//        _value = initialValue
-//        (producer, _valueObserver) = SignalProducer<[Value], NoError>.buffer(1)
-//        (changes, _changesObserver) = SignalProducer<CollectionChange<Value>, NoError>.buffer(1)
-//    }
-//    
-//    deinit {
-//        sendCompleted(_valueObserver)
-//        sendCompleted(_changesObserver)
-//    }
-//}
+public final class MutableCollectionProperty<T>: PropertyType {
+
+    public typealias Value = [T]
+
+    
+    // MARK: - Private attributes
+
+    private let _valueObserver: Signal<Value, NoError>.Observer
+    private let _changesObserver: Signal<CollectionChange<Value.Element>, NoError>.Observer
+    private var _value: Value
+    private let _lock = NSRecursiveLock()
+
+    // MARK: - Public Attributes
+
+    public var producer: SignalProducer<Value, NoError>
+    public var changes: SignalProducer<CollectionChange<Value.Element>, NoError>
+    public var value: Value {
+        get {
+            _lock.lock()
+            let value = _value
+            _lock.unlock()
+            return value
+        }
+        set {
+            _lock.lock()
+            _value = newValue
+            sendNext(_valueObserver, newValue)
+            sendNext(_changesObserver, .StartChange)
+            sendNext(_changesObserver, .Replacement(_value))
+            sendNext(_changesObserver, .EndChange)
+            _lock.unlock()
+        }
+    }
+
+    // MARK: - Init/Deinit
+
+    public init(_ initialValue: Value) {
+        _lock.name = "org.reactivecocoa.ReactiveCocoa.MutableCollectionProperty"
+        _value = initialValue
+        (producer, _valueObserver) = SignalProducer<Value, NoError>.buffer(1)
+        (changes, _changesObserver) = SignalProducer<CollectionChange<Value.Element>, NoError>.buffer(1)
+    }
+
+    deinit {
+        sendCompleted(_valueObserver)
+        sendCompleted(_changesObserver)
+    }
+    
+    
+    // MARK: - Public
+
+    public func removeFirst() {
+        if (_value.count == 0) { return }
+        let deletedElement = _value.removeFirst()
+        sendNext(_valueObserver, _value)
+        sendNext(_changesObserver, .StartChange)
+        sendNext(_changesObserver, CollectionChange.Deletion(0, deletedElement))
+        sendNext(_changesObserver, .EndChange)
+    }
+
+    public func removeLast() {
+        if (_value.count == 0) { return }
+        let index = _value.count - 1
+        let deletedElement = _value.removeLast()
+        sendNext(_valueObserver, _value)
+        sendNext(_changesObserver, .StartChange)
+        sendNext(_changesObserver, .Deletion(index, deletedElement))
+        sendNext(_changesObserver, .EndChange)
+    }
+
+    public func removeAtIndex(index: Int) {
+        let deletedElement = _value.removeAtIndex(index)
+        sendNext(_valueObserver, _value)
+        sendNext(_changesObserver, .StartChange)
+        sendNext(_changesObserver, CollectionChange.Deletion(index, deletedElement))
+        sendNext(_changesObserver, .EndChange)
+    }
+}
--- MutableCollectionPropertyTests/MutableCollectionPropertyTests.swift
+++ MutableCollectionPropertyTests/MutableCollectionPropertyTests.swift
@@ -7,30 +7,226 @@
 //
 
 import XCTest
+import Quick
+import Nimble
+import ReactiveCocoa
+
 @testable import MutableCollectionProperty
 
-class MutableCollectionPropertyTests: XCTestCase {
-    
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock {
-            // Put the code you want to measure the time of here.
+class MutableCollectionPropertyTests: QuickSpec {
+
+    override func spec() {
+
+        describe("initialization") {
+
+            it("should properly update the value once initialized") {
+                let array: [String] = ["test1, test2"]
+                let property: MutableCollectionProperty<String> = MutableCollectionProperty(array)
+                expect(property.value) == array
+            }
         }
+
+        describe("updates") {
+
+            context("full update") {
+
+                it("should notify the main producer") {
+                    let array: [String] = ["test1", "test2"]
+                    let property: MutableCollectionProperty<String> = MutableCollectionProperty(array)
+                    waitUntil(action: { (done) -> Void in
+                        property.producer.on(event: { event in
+                            switch event {
+                            case .Next(_):
+                                done()
+                            default: break
+                            }
+                        }).start()
+                        property.value = ["test2", "test3"]
+                    })
+                }
+
+                it("should notify the changes producer with the replaced enum type") {
+                    let array: [String] = ["test1", "test2"]
+                    let newArray: [String] = ["test2", "test3"]
+                    let property: MutableCollectionProperty<String> = MutableCollectionProperty(array)
+                    waitUntil(action: {
+                        (done) -> Void in
+                        var i: Int = 0
+                        property.changes.on(event: {
+                            event in
+                            switch event {
+                            case .Next(let change):
+                                switch change {
+                                case .StartChange:
+                                    expect(i) == 0
+                                case .Replacement(let newValue):
+                                    expect(newValue) == newArray
+                                    expect(i) == 1
+                                case .EndChange:
+                                    done()
+                                default: break
+                                }
+                                i++
+                            default: break
+                            }
+                        }).start()
+                        property.value = newArray
+                    })
+                }
+            }
+
+        }
+
+        describe("deletion") {
+
+            context("delete at a given index") {
+
+                it("should notify the main producer") {
+                    let array: [String] = ["test1", "test2"]
+                    let property: MutableCollectionProperty<String> = MutableCollectionProperty(array)
+                    waitUntil(action: {
+                        (done) -> Void in
+                        property.producer.on(event: {
+                            event in
+                            switch event {
+                            case .Next(let newValue):
+                                expect(newValue) == ["test1"]
+                                done()
+                            default: break
+                            }
+                        }).start()
+                        property.removeAtIndex(1)
+                    })
+                }
+
+                it("should notify the changes producer with the right type") {
+                    let array: [String] = ["test1", "test2"]
+                    let property: MutableCollectionProperty<String> = MutableCollectionProperty(array)
+                    waitUntil(action: {
+                        (done) -> Void in
+                        var i: Int = 0
+                        property.changes.on(event: {
+                            event in
+                            switch event {
+                            case .Next(let change):
+                                switch change {
+                                case .StartChange:
+                                    expect(i) == 0
+                                case .Deletion(let index, let element):
+                                    expect(i) == 1
+                                    expect(index) == 1
+                                    expect(element) == "test2"
+                                case .EndChange:
+                                    done()
+                                default: break
+                                }
+                            default: break
+                            }
+                            i++
+                        }).start()
+                        property.removeAtIndex(1)
+                    })
+                }
+            }
+            
+            context("deleting the last element", {
+                
+                it("should notify the deletion to the main producer") {
+                    let array: [String] = ["test1", "test2"]
+                    let property: MutableCollectionProperty<String> = MutableCollectionProperty(array)
+                    waitUntil(action: { (done) -> Void in
+                        property.producer.on(event: {
+                            event in
+                            switch event {
+                            case .Next(let change):
+                                expect(change) == ["test1"]
+                                done()
+                            default: break
+                            }
+                        }).start()
+                        property.removeLast()
+                    })
+                }
+                
+                it("should notify the deletion to the changes producer with the right type") {
+                    let array: [String] = ["test1", "test2"]
+                    let property: MutableCollectionProperty<String> = MutableCollectionProperty(array)
+                    waitUntil(action: { (done) -> Void in
+                        var i: Int = 0
+                        property.changes.on(event: {
+                            event in
+                            switch event {
+                            case .Next(let change):
+                                switch change {
+                                case .StartChange:
+                                    expect(i) == 0
+                                case .Deletion(let index, let element):
+                                    expect(i) == 1
+                                    expect(index) == 1
+                                    expect(element) == "test2"
+                                case .EndChange:
+                                    done()
+                                default: break
+                                }
+                            default: break
+                            }
+                            i++
+                        }).start()
+                        property.removeLast()
+                    })
+                }
+                
+            })
+            
+            context("deleting the first element", {
+                it("should notify the deletion to the main producer") {
+                    let array: [String] = ["test1", "test2"]
+                    let property: MutableCollectionProperty<String> = MutableCollectionProperty(array)
+                    waitUntil(action: { (done) -> Void in
+                        property.producer.on(event: {
+                            event in
+                            switch event {
+                            case .Next(let change):
+                                expect(change) == ["test2"]
+                                done()
+                            default: break
+                            }
+                        }).start()
+                        property.removeFirst()
+                    })
+                }
+                
+                it("should notify the deletion to the changes producer with the right type") {
+                    let array: [String] = ["test1", "test2"]
+                    let property: MutableCollectionProperty<String> = MutableCollectionProperty(array)
+                    waitUntil(action: { (done) -> Void in
+                        var i: Int = 0
+                        property.changes.on(event: {
+                            event in
+                            switch event {
+                            case .Next(let change):
+                                switch change {
+                                case .StartChange:
+                                    expect(i) == 0
+                                case .Deletion(let index, let element):
+                                    expect(i) == 1
+                                    expect(index) == 0
+                                    expect(element) == "test1"
+                                case .EndChange:
+                                    done()
+                                default: break
+                                }
+                            default: break
+                            }
+                            i++
+                        }).start()
+                        property.removeFirst()
+                    })
+                }
+            })
+
+        }
+
     }
-    
+
 }
